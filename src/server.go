1f571eaa2b46746eb581b7157a75b50e7506d8f9
maheshhp <hpmahesh73@gmail.com>
2018-09-29 14:25:37 +0530

    Setup index and stop routes

--- src/server.go
+++ src/server.go
@@ -1,9 +1,15 @@
 package main
 
-import "fmt"
-import "github.com/go-redis/redis"
 import (
+	"context"
+	"fmt"
+	"io"
+
+	"github.com/go-redis/redis"
 	"github.com/jinzhu/gorm"
+
+	"net/http"
+
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
@@ -34,6 +40,19 @@ func RedisClient() *redis.Client {
 	return client
 }
 
+// IndexHandler -> Handles requests coming to / route
+func IndexHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Welcome!\n")
+}
+
+// StopHandler -> Stops the server on request to /stop route
+func StopHandler(w http.ResponseWriter, req *http.Request, dbClient *gorm.DB, redisClient *redis.Client, serverInstance *http.Server) {
+	fmt.Println("Stopping server...\n")
+	dbClient.Close()
+	redisClient.Close()
+	serverInstance.Shutdown(context.TODO())
+}
+
 func main() {
 	redisClient := RedisClient()
 
@@ -45,7 +64,15 @@ func main() {
 
 	dbClient.AutoMigrate(&urls{})
 
-	dbClient.Create(&urls{Tinyurl: "test.tiny", Longurl: "test.t"})
+	serverInstance := &http.Server{
+		Addr: ":8080",
+	}
 
-	dbClient.Close()
+	http.HandleFunc("/", IndexHandler)
+
+	http.HandleFunc("/stop", func(w http.ResponseWriter, r *http.Request) {
+		StopHandler(w, r, dbClient, redisClient, serverInstance)
+	})
+
+	serverInstance.ListenAndServe()
 }
